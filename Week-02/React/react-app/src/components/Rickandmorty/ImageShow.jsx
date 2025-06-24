6a6bf796bb0d2b5097c0342b7d946221549e980a
Jidnya Ganesh Mahajan <jidnya.mahajan@mindbowser.com>
2025-06-24 17:58:44 +0530

    Node: MVC

--- Week-02/React/react-app/src/components/Rickandmorty/ImageShow.jsx
+++ Week-02/React/react-app/src/components/Rickandmorty/ImageShow.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 function ImageShow({ character }) {
   return (
     <div className="image-card">
+      <div className="card-details">
+        <h3>{character.name}</h3>
+        <p><strong>Status:</strong> {character.status}</p>
+        <p><strong>Species:</strong> {character.species}</p>
+        <p><strong>Origin:</strong> {character.origin.name}</p>
+      </div>
       <img src={character.image} alt={character.name} />
-      <h3>{character.name}</h3>
-      <p><strong>Status:</strong> {character.status}</p>
-      <p><strong>Species:</strong> {character.species}</p>
-      <p><strong>Origin:</strong> {character.origin.name}</p>
     </div>
   );
 }
--- Week-02/React/react-app/src/components/Rickandmorty/index.jsx
+++ Week-02/React/react-app/src/components/Rickandmorty/index.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import SearchBar from './SearchBar';
-import Image from './Image';
-import './ImageStyle.css';
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import Image from "./Image";
+import "./ImageStyle.css";
 
 function RickAndMortyApp() {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
   const [hasSearched, setHasSearched] = useState(false);
 
   const handleSearch = (value) => {
@@ -13,18 +13,27 @@ function RickAndMortyApp() {
   };
 
   return (
-    <div className="app-wrapper">
-    <div >
-      <div className="search-wrapper">
-        <SearchBar onSubmit={handleSearch} />
+    <div className="main">
+      <div className="heading">
+         <h2> The Rick & Morty</h2>
       </div>
+      <h2 style={{ color: "black", textAlign: "center" }}>Welcome to the Multiverse Explorer!</h2>
+      <h3 style={{ color: "black", textAlign: "center" }}>Search for your favorite Rick and Morty characters using the bar above.</h3>
 
-      {hasSearched && (
-        <div className="image-results">
-          <Image term={term} />
+
+      <div className="app-wrapper">
+        <div>
+          <div className="search-wrapper">
+            <SearchBar onSubmit={handleSearch} />
+          </div>
+
+          {hasSearched && (
+            <div className="image-results">
+              <Image term={term} />
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
     </div>
   );
 }
